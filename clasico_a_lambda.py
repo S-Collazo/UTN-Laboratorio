fb9ded45022b9e3c12f738cd74968a52a7a0723a
Santiago Collazo <santiago.collazo7@gmail.com>
2022-10-27 19:41:07 -0300

    commit

--- clasico_a_lambda.py
+++ clasico_a_lambda.py
@@ -21,58 +21,64 @@
 # SOFTWARE.
 
 from random import randint
-
+from random import shuffle
+from functools import reduce
 
 lista_palabras = [
     "Goku", "Vegeta", "Frieza", "Cell", "Beerus", 'Krillin'
 ]
 
 # Refactoprizar la funcion clasica usando lambda
-def sup_triangulo(base: int, altura: int) -> float:
-    return (base*altura)/2
-
+# def sup_triangulo(base: int, altura: int) -> float:  
+#    return (base*altura)/2
+sup_triangulo = lambda base,altura : base*altura/2
 
 # Refactorizar la funcion clasica usando lambda y map
-def aplicar_mayusculas(lista_palabras: list) -> list:
-    for i in range(len(lista_palabras)):
-        lista_palabras[i] = lista_palabras[i].upper()
-    return lista_palabras
+# def aplicar_mayusculas(lista_palabras: list) -> list:
+#    for i in range(len(lista_palabras)):
+#        lista_palabras[i] = lista_palabras[i].upper()
+#    return lista_palabras
+aplicar_mayusculas = lambda lista : list(map(str.upper, lista))
 
 # Refactorizar la funcion usando lambda y reduce
-def obtener_mas_letras(lista: list) -> str:
-    seleccionado = ''
-    for palabra in lista:
-        if len(palabra) > len(seleccionado):
-            seleccionado = palabra
-    return seleccionado
+#def obtener_mas_letras(lista: list) -> str:
+#    seleccionado = ''
+#    for palabra in lista:
+#        if len(palabra) > len(seleccionado):
+#            seleccionado = palabra
+#    return seleccionado
+obtener_mas_letras = reduce(lambda palabra1, palabra2 : palabra1 if len(palabra1) > len(palabra2) else palabra2, lista)
 
-# refactorizar la funcion usando lambda y filter
-def obtener_nombres_cantidad_letras(lista: list, cantidad: int) -> list:
-    seleccionados = list()
-    for palabra in lista:
-        if len(palabra) == cantidad:
-            seleccionados.insert(0, palabra)
-    return seleccionados
+# Refactorizar la funcion usando lambda y filter
+#def obtener_nombres_cantidad_letras(lista: list, cantidad: int) -> list:
+#    seleccionados = list()
+#    for palabra in lista:
+#        if len(palabra) == cantidad:
+#            seleccionados.insert(0, palabra)
+#    return seleccionados
+obtener_nombres_cantidad_letras = list(filter(lambda palabra : len(palabra) == 4, lista_palabras))
 
-# refactorizar usando shuffle
+# Refactorizar usando shuffle
+#def ordenar_random_lista(lista: list) -> list:
+#    maximo = len(lista)
+#    desordenada = list()
+#    while len(desordenada) < len(lista):
+#        indice = randint(0, maximo)
+#        for elemento in lista:
+#            desordenada.insert(indice, elemento)
+#    return desordenada
 def ordenar_random_lista(lista: list) -> list:
-    maximo = len(lista)
-    desordenada = list()
-    while len(desordenada) < len(lista):
-        indice = randint(0, maximo)
-        for elemento in lista:
-            desordenada.insert(indice, elemento)
-    return desordenada
+    shuffle(lista)
 
 # Refactorizar usando sort y lamda
-def ordenar_burbujeo(lista: list) -> list:
-    lista_copia = lista.copy()
-    for i in range(len(lista_copia)-1):
-        for j in range(i+1, len(lista_copia)):
-            if lista_copia[i] > lista_copia[j]:
-                lista_copia[i], lista_copia[j] = lista_copia[j], lista_copia[i]
-    return lista_copia
-
+#def ordenar_burbujeo(lista: list) -> list:
+#    lista_copia = lista.copy()
+#    for i in range(len(lista_copia)-1):
+#        for j in range(i+1, len(lista_copia)):
+#            if lista_copia[i] > lista_copia[j]:
+#                lista_copia[i], lista_copia[j] = lista_copia[j], lista_copia[i]
+#    return lista_copia
+ordenar_burbujeo = lambda lista_palabras : lista_palabras.sort()
 
 heroes = [
     "goKU", "vEgETa", 'kriLLin'
@@ -87,15 +93,23 @@ ataques = [
 ]
 
 # Refactorizar usando zip
-for ind_h in range(len(heroes)):
-    for ind_a in range(ind_h, len(ataques)):
-        for ind_v in range(ind_h, len(villanos)):
-            mensaje =\
-                f"""
-                {heroes[ind_h].capitalize()}
-                Lanza un {ataques[ind_a].capitalize()}
-                a {villanos[ind_v].capitalize()}
-                """
-            print(mensaje)
-            break
-        break
+#for ind_h in range(len(heroes)):
+#    for ind_a in range(ind_h, len(ataques)):
+#        for ind_v in range(ind_h, len(villanos)):
+#            mensaje =\
+#                f"""
+#                {heroes[ind_h].capitalize()}
+#                Lanza un {ataques[ind_a].capitalize()}
+#                a {villanos[ind_v].capitalize()}
+#                """
+#            print(mensaje)
+#            break
+#        break
+
+def imprimir_mensaje():
+    for heroe, ataque, villano in zip(heroes, ataques, villanos):
+        mensaje = "{0} \nLanza un {1} \na {2}".format(heroe.capitalize(),ataque.capitalize(),villano.capitalize())
+        print(mensaje)
+    
+    
+print(obtener_mas_letras(lista_palabras))
