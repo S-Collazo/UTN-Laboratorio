2115ddbc0c08d3cb94aee1f5b0e3dd2b93951601
Santiago Collazo <santiago.collazo7@gmail.com>
2022-12-01 22:01:25 -0300

    commit

--- pygame-2/ui_highscore.py
+++ pygame-2/ui_highscore.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 from constants import *
 from ui_highscore_main import HighscoreMain
 from ui_highscore_table import HighscoreTable
+from ui_highscore_register import HighscoreRegister
 
 class Highscore:
     def __init__ (self,screen):
@@ -14,6 +15,7 @@ class Highscore:
         
         self.highscore_main = HighscoreMain(name="highscore_main",master_surface=screen,x=0,y=0,w=ANCHO_VENTANA,h=ALTO_VENTANA,background_color=None,border_color=None,active=True)
         self.highscore_table = HighscoreTable(name="highscore_table",master_surface=screen,x=0,y=0,w=ANCHO_VENTANA,h=ALTO_VENTANA,background_color=None,border_color=None,active=False)
+        self.highscore_register = HighscoreRegister(name="highscore_register",master_surface=screen,x=300,y=300,w=300,h=100,background_color=BLACK,border_color=None,active=False)
         
         self.score = 0
         self.exit = False
@@ -34,9 +36,13 @@ class Highscore:
             self.score = self.highscore_main.final
             self.exit = self.highscore_main.exit
         elif(self.highscore_table.active):
-            self.highscore_table.update(lista_eventos,self.score)
+            self.highscore_table.update(lista_eventos)
             self.highscore_table.draw()
             self.exit = self.highscore_table.exit
+        elif(self.highscore_register.active):
+            self.highscore_register.update(lista_eventos,self.score)
+            self.highscore_register.draw()
+            self.exit = self.highscore_register.exit
         else:
             if (self.exit):
                 self.game_state = GAME_MENU
